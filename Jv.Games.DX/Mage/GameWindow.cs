ca05064276e784181bffd617d28e76951197199b
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-04-15 23:52:00 -0300

    Code cleanup

--- Jv.Games.DX/Mage/GameWindow.cs
+++ Jv.Games.DX/Mage/GameWindow.cs
@@ -1,14 +1,8 @@
-﻿using Jv.Games.DX.Test;
-using SharpDX.Direct3D9;
+﻿using SharpDX.Direct3D9;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using WinAPI;
 
 namespace Mage
 {
